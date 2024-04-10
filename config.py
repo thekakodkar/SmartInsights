e67c14db5c09cdc0a6c7c1bb73b83432397fa990
Niraj Kakodkar <niraj.kakodkar@gmail.com>
2024-04-10 11:42:44 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+os.environ["YOUR_LLM_API_KEY"] = ""
+jdbc_url = "jdbc:://"
+username = "your_username"
+password = "your_password"
+driver_path = ""
+custom_icon_path = ""
+logo_url = ""
